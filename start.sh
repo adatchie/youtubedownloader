d1fc8e06d1fde35ae519ed888843dbf944154f2a
adatchie <adatchie@gmail.com>
2026-08-22 16:14:26 +0900

    Fix shell script line endings

--- start.sh
+++ start.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 set -eu
 
+# Deployment images are built on Windows hosts; normalize CRLF before exec.
+if grep -q $'\r' "$0"; then
+    tr -d '\r' < "$0" > /tmp/start.sh
+    chmod +x /tmp/start.sh
+    exec sh /tmp/start.sh "$@"
+fi
+
 node /opt/bgutil-provider/build/main.js --port 4416 &
 provider_pid=$!
 
